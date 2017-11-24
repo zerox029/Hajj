1d630929ffd71c32fce851faa09f7a9bb50805cb
zerox029 <zerox029@gmail.com>
2017-11-24 12:37:43 -0500

    Base Commit

---
+++ Assets/Scripts/Dice.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Dice : MonoBehaviour {
+
+    Text numberTxt;
+
+    [SerializeField]
+    PlayerMovement playerMove;
+    [SerializeField]
+    GameManager manager;
+
+    private void Start()
+    {
+        numberTxt = GameObject.FindGameObjectWithTag("DiceNumber").GetComponent<Text>();
+    }
+
+    public void RollDice()
+    {
+        int random = Random.Range(1, 7);
+        int destTile = playerMove.currentTile + random;
+
+        numberTxt.text = random.ToString();
+        StartCoroutine(playerMove.MovePlayer(destTile, playerMove.transform, manager.tiles[destTile].transform, 1.0f));
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public GameManager manager;
+    public int currentTile;
+
+    private float speed = 3.0f;
+
+    private void Start()
+    {
+        currentTile = 0;
+        transform.position = manager.tiles[currentTile].transform.position;
+    }
+
+    public IEnumerator MovePlayer(int tileNumber, Transform source, Transform target, float overtime)
+    {
+        float startTime = Time.time;
+        while(Time.time < startTime + overtime)
+        {
+            transform.position = Vector3.Lerp(source.position, target.position, (Time.time - startTime) / overtime);
+            yield return null;
+        }
+
+        transform.position = target.position;
+
+        currentTile = tileNumber;
+    }
+}
