06bdbcd08b8b0ea8c736279b07f4f41ff5290e76
zerox029 <zerox029@gmail.com>
2017-12-04 20:51:50 -0500

    Completed implementation of JSON with question system

--- Assets/Scripts/Gameplay/Question.cs
+++ Assets/Scripts/Gameplay/Question.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
-using System.IO;
+using UnityEngine.EventSystems;
 
 public class Question : MonoBehaviour {
 
@@ -12,56 +13,75 @@ public class Question : MonoBehaviour {
     public Color wrongColor;
     public Color rightColor;
 
-    private PlayerMovement playerMove;
+    PlayerMovement playerMove;
+    QuestionInfo test;
 
+    Text diffText;
+    Text rewardText;
     Text questionTitle;
-    GameObject optionA;
-    GameObject optionB;
-    GameObject optionC;
-    GameObject optionD;
+    GameObject[] options;
 
     private void Start()
     {
         jsonPath = Application.streamingAssetsPath + "/questions.json";
         jsonString = File.ReadAllText(jsonPath);
-        QuestionInfo test = JsonUtility.FromJson<QuestionInfo>(jsonString);
+        test = JsonUtility.FromJson<QuestionInfo>(jsonString);
 
         questionTitle = transform.Find("Question").GetComponent<Text>();
         questionTitle.text = test.question;
 
-        optionA = transform.Find("AnswerA").gameObject;
-        optionB = transform.Find("AnswerB").gameObject;
-        optionC = transform.Find("AnswerC").gameObject;
-        optionD = transform.Find("AnswerD").gameObject;
+        options = new GameObject[] { transform.Find("AnswerA").gameObject,
+                                     transform.Find("AnswerB").gameObject,
+                                     transform.Find("AnswerC").gameObject,
+                                     transform.Find("AnswerD").gameObject };
 
-        optionA.GetComponentInChildren<Text>().text = test.options[0];
-        optionB.GetComponentInChildren<Text>().text = test.options[1];
-        optionC.GetComponentInChildren<Text>().text = test.options[2];
-        optionD.GetComponentInChildren<Text>().text = test.options[3];
+        options[0].GetComponentInChildren<Text>().text = test.options[0];
+        options[1].GetComponentInChildren<Text>().text = test.options[1];
+        options[2].GetComponentInChildren<Text>().text = test.options[2];
+        options[3].GetComponentInChildren<Text>().text = test.options[3];
 
-        for(int i = 0; i<test.difficulty.Length; i++)
+        //Setting the buttons
+        for(int i = 0; i<test.options.Length; i++)
         {
-            if(i == test.answer)
+            if(i == test.answer - 1)
             {
-                //Add correct answer behavior
+                options[i].GetComponent<Button>().onClick.AddListener(rightAnswer);
             }
             else
             {
-                //Add wrong answer behavior
+                options[i].GetComponent<Button>().onClick.AddListener(wrongAnswer);
             }
         }
+
+        diffText = transform.Find("Difficulty").GetComponent<Text>();
+        diffText.text = "Difficulty: " + test.difficulty;
+        rewardText = transform.Find("Money").GetComponent<Text>();
+        rewardText.text =  test.reward.ToString() + "$";
+
     }
 
     public void wrongAnswer()
     {
-        GetComponent<Image>().color = wrongColor;
+        EventSystem.current.currentSelectedGameObject.GetComponent<Image>().color = wrongColor;
+
+        if(test.reward > 100)
+        {
+            test.reward -= 100;
+        }
+
+        else
+        {
+            test.reward = 0;
+        }
+
+        rewardText.text = test.reward.ToString() + "$";
     }
 
     public void rightAnswer()
     {
         playerMove = GameObject.FindGameObjectWithTag("Manager").GetComponent<Dice>().playerMove;
 
-        GetComponent<Image>().color = rightColor;
+        EventSystem.current.currentSelectedGameObject.GetComponent<Image>().color = wrongColor;
         Destroy(GameObject.FindGameObjectWithTag("QPanel"));
         Destroy(GameObject.FindGameObjectWithTag("Blur"));
 
@@ -75,4 +95,5 @@ public class QuestionInfo
     public string difficulty;
     public string[] options;
     public int answer;
+    public int reward;
 }
